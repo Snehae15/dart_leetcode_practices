97af1683ca79d7fba4db027421f6247e955231d5
Snehae15 <snehae15@gmail.com>
2024-08-27 11:52:19 +0530

    updation

--- lib/leetcode/1.Two Sum.dart
+++ lib/leetcode/1.Two Sum.dart
@@ -38,10 +38,7 @@ class Solution {
     for (var i = 0; i < n - 1; i++) {
       // for loop for keep tracking the second value inside the list
       for (var j = i + 1; j < n; j++) {
-        // first of any value inside the list and second inside the
-        // list should be equal to target
         if (numbers[i] + numbers[j] == target) {
-          // return the list of first value and second
           return [i, j];
         }
       }
